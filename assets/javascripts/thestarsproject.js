2d217477855a2ebd01529bc153913d74bafe19d2
Julia Camille Menchavez <jcmenchavez@gmail.com>
2014-10-20 10:19:09 +0800

    parsed read data into javascript objects

--- assets/javascripts/thestarsproject.js
+++ assets/javascripts/thestarsproject.js
@@ -10,12 +10,21 @@ require(['jquery', 'gradient-descent'], function($, GradientDescent) {
         var file = this.files[0];
         var reader = new FileReader();
         reader.onload = function(e) {
-            // @todo parse read data to javascript objects
-            //       note: read data is in e.target.result
+            var data = e.target.result.split('\n');
+            var training_data = [];
+            for (var i = 0; i < data.length; i++) {
+                var line = data[i].replace(/ +/g, ' ').split(' ');
+                var object = {
+                    name: line.slice(0, line.length - 3).join(' '),
+                    features: [parseFloat(line[line.length - 2].replace(/v|e/g, '')), parseFloat(line[line.length - 1])],
+                    label: parseFloat(line[line.length - 3].replace(/v|e/g, ''))
+                };
+                training_data.push(object);
+            }
+            console.log(training_data);
         };
         reader.readAsText(file);
     });
-
     var gd = new GradientDescent({ features: 2, cost_threshold: 0.01, alpha: 0.1 });
     gd.train();
 });
