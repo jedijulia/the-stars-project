50dddd494ae6ceb92b05e1a3fa35888f7147ae53
Arnelle Balane <arnellebalane@gmail.com>
2014-10-20 09:21:53 +0800

    filereader to read input data

--- assets/javascripts/thestarsproject.js
+++ assets/javascripts/thestarsproject.js
@@ -6,5 +6,16 @@ require.config({
 });
 
 require(['jquery', 'gradient-descent'], function($, GradientDescent) {
-    
+    $('#input').on('change', function() {
+        var file = this.files[0];
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            // @todo parse read data to javascript objects
+            //       note: read data is in e.target.result
+        };
+        reader.readAsText(file);
+    });
+
+    var gd = new GradientDescent({ features: 2, cost_threshold: 0.01, alpha: 0.1 });
+    gd.train();
 });
